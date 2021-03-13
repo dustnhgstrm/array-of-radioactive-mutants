ddac347b4ebcc9a07af424dc6d622e48841c73ce
Dustin Hagstrom <dustin.l.hagstrom@gmail.com>
2021-03-13 17:03:44 -0500

    ade functions to meet req's of the tests.

--- main.js
+++ main.js
@@ -2,39 +2,68 @@
  * YOUR CODE HERE *
  ******************/
 
+function addMeToEnd(arr) {
+  return arr.push("Colin");
+}
 
+function addMeToStart(arr) {
+  return arr.unshift("Colin");
+}
 
+function changeLast(arr, value) {
+  arr.pop();
+  arr.push(value);
+}
 
+function changeAllValuesTo(arr, value) {
+  for (i = 0; i < arr.length; i++) {
+    arr[i] = value;
+  }
+}
+
+function oddOrEven(arr) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arr[i] = "even";
+    } else {
+      arr[i] = "odd";
+    }
+  }
+}
+
+function changeNextThreeToValue(index, arr, value) {
+  for (let i = index; i < index + 3; i++) {
+    arr[i] = value;
+  }
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
 
-
-if (typeof addMeToEnd === 'undefined') {
+if (typeof addMeToEnd === "undefined") {
   addMeToEnd = undefined;
 }
 
-if (typeof addMeToStart === 'undefined') {
+if (typeof addMeToStart === "undefined") {
   addMeToStart = undefined;
 }
 
-if (typeof changeLast === 'undefined') {
+if (typeof changeLast === "undefined") {
   changeLast = undefined;
 }
 
-if (typeof changeAllValuesTo === 'undefined') {
+if (typeof changeAllValuesTo === "undefined") {
   changeAllValuesTo = undefined;
 }
 
-if (typeof oddOrEven === 'undefined') {
+if (typeof oddOrEven === "undefined") {
   oddOrEven = undefined;
 }
 
-if (typeof changeNextThreeToValue === 'undefined') {
+if (typeof changeNextThreeToValue === "undefined") {
   changeNextThreeToValue = undefined;
 }
-
 
 module.exports = {
   addMeToEnd,
@@ -43,4 +72,4 @@ module.exports = {
   changeAllValuesTo,
   oddOrEven,
   changeNextThreeToValue,
-}
+};
